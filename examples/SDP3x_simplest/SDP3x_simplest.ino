cf3d888008e2ef0c77bff69a6bf54d615c1ee65e
Johannes Winkelmann <jw@smts.ch>
2018-06-13 08:37:53 -0700

    add missing semicolons

--- examples/SDP3x_simplest/SDP3x_simplest.ino
+++ examples/SDP3x_simplest/SDP3x_simplest.ino
@@ -15,11 +15,11 @@ void setup() {
 // the loop routine runs over and over again forever
 void loop() {
 	// Simply print raw value, this can be viewed in the serial plotter
-	Serial.print("Pressure difference: ")
+	Serial.print("Pressure difference: ");
 	Serial.println(SDP3x.getPressureDiff());
 	// delay for a 100ms until the next sensor reading
 	delay(100);
-	Serial.print("Temperature: ")
+	Serial.print("Temperature: ");
 	Serial.println(SDP3x.getTemperature());
 	// delay for a 100ms until the next sensor reading
 	delay(100);
